c375c80ca50f826faf3a8db80946dd0b9a6c409e
DarKray33 <alejandroarcealzate@hotmail.com>
2014-11-15 15:11:20 -0500

    Objeto pokemon
    definicion de la clase constructora pokemon y creacion de charizard y
    newtwo con sus respectivas estadisticas base

--- js/game.js
+++ js/game.js
@@ -1,11 +1,16 @@
-var Pokemon = function(v, a)
+var Pokemon = function(t, hp, atk, def, spatk, spdef, speed)
 {
-	this.hp = v;
-	this.ataque = a;
+	this.tipe = t;
+	this.hp = hp;
+	this.atk= atk;
+	this.defense = def;
+	this.spatk = spatk;
+	this.spdef = spdef;
+	this.speed = speed;
 }
 
-var Charizard = new Pokemon(1000, 100);
-var Newtwo = new Pokemon(2000, 200);
+var Charizard = new Pokemon("Fuego-Dragon", 78, 130, 111, 130, 85, 100);
+var Newtwo = new Pokemon("Psiquico", 106, 110, 90, 154, 90, 130);
 
 CharizardHP.innerText = "HP: " + Charizard.hp;
 NewtwoHP.innerText = "HP: " + Newtwo.hp;
