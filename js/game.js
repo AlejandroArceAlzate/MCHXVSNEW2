1727b0fd6048c5ad0cb2e6884de227a851e099cf
DarKray33 <alejandroarcealzate@hotmail.com>
2014-11-15 17:17:14 -0500

    Datos basicos del pokemon
    Definicion de la funcion que se encargara de imprimir los datos basicos
    de el respectivo pokemon en la aplicacion

--- js/game.js
+++ js/game.js
@@ -1,16 +1,49 @@
-var Pokemon = function(t, hp, atk, def, spatk, spdef, speed)
+var Pokemon = function(t, l, hp, atk, def, spatk, spdef, speed)
 {
-	this.tipe = t;
-	this.hp = hp;
-	this.atk= atk;
-	this.defense = def;
-	this.spatk = spatk;
-	this.spdef = spdef;
-	this.speed = speed;
+	this.Tipe = t;
+	this.Level= l;
+	this.Hp = hp;
+	this.Atk= atk;
+	this.Defense = def;
+	this.Spatk = spatk;
+	this.Spdef = spdef;
+	this.Speed = speed;
+
 }
 
-var Charizard = new Pokemon("Fuego-Dragon", 78, 130, 111, 130, 85, 100);
-var Newtwo = new Pokemon("Psiquico", 106, 110, 90, 154, 90, 130);
+function imprimirDatosPokemon()
+{
+	var Charizard = new Pokemon("Fuego-Dragon", 100, 360, 394, 353, 394, 295, 328);
+	var Mewtwo = new Pokemon("Psiquico", 100, 416, 350, 306, 447, 306, 394);
 
-CharizardHP.innerText = "HP: " + Charizard.hp;
-NewtwoHP.innerText = "HP: " + Newtwo.hp;
+	$("#NombreCharizard").text("Charizard");
+	$("#CharizardHP").text("HP: " + Charizard.Hp);
+	$("#CharizardLVL").text("LVL: " + Charizard.Level);
+	$("#CharizardTipo").text("Tipo: " + Charizard.Tipe);
+
+	$("#NombreMewtwo").text("Mewtwo");
+	$("#MewtwoHP").text("HP: " + Mewtwo.Hp);
+	$("#MewtwoLVL").text("LVL: " + Mewtwo.Level);
+	$("#MewtwoTipo").text("Tipo: " + Mewtwo.Tipe);
+
+
+
+}
+
+var ataque = function(N, P, Prec, PPA, PPT)
+	{
+		this.Nombre = N;
+		this.Potencia = P;
+		this.Precision = Prec;
+		this.PPactual = PPA;
+		this.PPtotal = PPT;
+	}
+
+var llamarada = new ataque("Fire Blast", 100, 85, 5, 5);
+
+ataquenombre.innerText = llamarada.Nombre;
+ppactual.innerText = llamarada.PPactual;
+pptotal.innerText = "/" + llamarada.PPtotal;
+
+
+$(document).ready(imprimirDatosPokemon());
