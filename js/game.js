3212fb126975273f8f0f5ec3422c69ce399ace32
DarKray33 <alejandroarcealzate@hotmail.com>
2014-11-15 19:02:26 -0500

    Comportamiento del ataque
    Prueba del comportamiento Basico de los ataques, quitandole vida a
    Mewtwo al apretar un ataque

--- js/game.js
+++ js/game.js
@@ -11,10 +11,24 @@ var Pokemon = function(t, l, hp, atk, def, spatk, spdef, speed)
 
 }
 
+var ataque = function(N, P, Prec, PPA, PPT)
+{
+	this.Nombre = N;
+	this.Potencia = P;
+	this.Precision = Prec;
+	this.PPactual = PPA;
+	this.PPtotal = PPT;
+}
+
+var Charizard = new Pokemon("Fuego-Dragon", 100, 360, 394, 353, 394, 295, 328);
+var Mewtwo = new Pokemon("Psiquico", 100, 416, 350, 306, 447, 306, 394);
+
+var llamarada = new ataque("FireBlast", 100, 85, 5, 5);
+var cuchillada = new ataque("Slash", 70, 100, 20, 20);
+
 function imprimirDatosPokemon()
 {
-	var Charizard = new Pokemon("Fuego-Dragon", 100, 360, 394, 353, 394, 295, 328);
-	var Mewtwo = new Pokemon("Psiquico", 100, 416, 350, 306, 447, 306, 394);
+	
 
 	$("#NombreCharizard").text("Charizard");
 	$("#CharizardHP").text("HP: " + Charizard.Hp);
@@ -26,24 +40,37 @@ function imprimirDatosPokemon()
 	$("#MewtwoLVL").text("LVL: " + Mewtwo.Level);
 	$("#MewtwoTipo").text("Tipo: " + Mewtwo.Tipe);
 
-
-
 }
 
-var ataque = function(N, P, Prec, PPA, PPT)
+
+function imprimirAtaques()
+{
+	$("#ataque1nombre").text(llamarada.Nombre);
+	$("#pp1actual").text(llamarada.PPactual);
+	$("#pp1total").text("/" + llamarada.PPtotal);
+
+	$("#ataque2nombre").text(cuchillada.Nombre);
+	$("#pp2actual").text(cuchillada.PPactual);
+	$("#pp2total").text("/" + cuchillada.PPtotal);
+}
+
+function atacarConLlamarada()
+{
+	var daño = 200;
+
+	Mewtwo.Hp -= Math.round(daño);
+
+	if(Mewtwo.Hp <= 0)
 	{
-		this.Nombre = N;
-		this.Potencia = P;
-		this.Precision = Prec;
-		this.PPactual = PPA;
-		this.PPtotal = PPT;
+		Mewtwo.Hp = "Fainted";
+		$("#MewtwoHP").text("HP: " + Mewtwo.Hp);
+		alert("Haz Derrotado a Mewtwo, FELICIDADES!!!!");
 	}
 
-var llamarada = new ataque("Fire Blast", 100, 85, 5, 5);
+	imprimirDatosPokemon();
+}
 
-ataquenombre.innerText = llamarada.Nombre;
-ppactual.innerText = llamarada.PPactual;
-pptotal.innerText = "/" + llamarada.PPtotal;
+imprimirDatosPokemon();
+imprimirAtaques();
 
-
-$(document).ready(imprimirDatosPokemon());
+$(".ataque1").on("click", atacarConLlamarada);
